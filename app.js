64b97f52226ff29a06d7ace11cdc47e72cb51d8e
guanqingchao <guanqingchao@diidglobal.com>
2019-11-06 14:56:35 +0800

    添加环境变量设置&node热更新

--- app.js
+++ app.js
@@ -1,48 +1,14 @@
-const http = require('http');
-const querystring = require('querystring');
-
-
-const server = http.createServer((req, res) => {
-    const {
-        url,
-        method
-    } = req;
-    const path = url.split('?')[0];
-    const query = querystring.parse(url.split('?')[1])
+const serverHandle = function (req, res) {
 
     //设置数据格式
     res.setHeader('Content-type', 'application-json');
 
     const resData = {
-        url,
-        path,
-        query,
-        method
+        env: process.env.NODE_ENV,
+        title: 'this is environment'
     }
+    res.end(JSON.stringify(resData))
 
-    if (method === "GET") {
-        res.end(JSON.stringify(resData))
-    }
+}
 
-    if (method === "POST") {
-
-        let postdata = '';
-        req.on('data', chunk => {
-            postdata += chunk.toString()
-        })
-
-        req.on('end', () => {
-            resData.postdata = postdata
-
-            res.end(
-                JSON.stringify(resData)
-            )
-        })
-    }
-
-
-
-})
-
-server.listen(8000)
-console.log('--------ok---------')
+module.exports = serverHandle
